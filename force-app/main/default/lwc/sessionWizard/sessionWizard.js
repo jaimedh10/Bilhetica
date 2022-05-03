9a7ffcf8c7f3ef3f6a208456da346e466618f147
jaimedh10 <jaime.oliveira@ubi.pt>
2022-05-03 18:10:33 +0100

    03/05

--- force-app/main/default/lwc/sessionWizard/sessionWizard.js
+++ force-app/main/default/lwc/sessionWizard/sessionWizard.js
@@ -11,6 +11,7 @@ import createSessionVenues from "@salesforce/apex/SessionWizardController.create
  */
 export default class SessionWizard extends LightningElement {
   @api recordId; // Id do Evento
+  isLoading = false;
   // Objectos e campos
   sessionObject = SESSION_OBJECT;
   sessionVenueObject = SESSION_VENUE_OBJECT;
@@ -20,10 +21,10 @@ export default class SessionWizard extends LightningElement {
   venueSelected;
   venueListEmpty;
   showSessionsSaved = false;
-  showTemplateSesssionVenues = false;
+  showCloningModal = true;
   // Listas
   sessions = undefined;
-  sessionVenues;
+  sessionVenues = undefined;
   venues;
   // Ids
   venueId;
@@ -32,7 +33,35 @@ export default class SessionWizard extends LightningElement {
 
   queryTerm = "";
   error;
+  peridiocityValue;
+  peridiocityOptions = [ 
+    {label: 'Daily', value: 'Daily' }, 
+    {label: 'Weekly', value: 'Weekly' },
+    {label: 'Monthly', value: 'Monthly' },
+    {label: 'Annualy', value: 'Annualy' }
+  ];
 
+
+
+  toggleCloningModal() {
+    this.showCloningModal = !this.showCloningModal;
+  }
+
+
+  cloneSessions() {
+    if(this.peridiocityValue == null || this.peridiocityValue === undefined || this.peridiocityValue === '' ) {
+      this.dispatchToast('Warning', 'Select peridiocity to clone Sessions', 'info');
+    }
+    else {
+      return null;
+    }
+    return null;
+  }
+
+  handlePeridiocityChange(event) {
+    this.peridiocityValue = event.detail.value;
+  } 
+  
   // Método para pesquisar o espaço com base no termo de pesquisa introduzido
   handleSearch(event) {
     this.queryTerm = event.target.value;
@@ -52,12 +81,13 @@ export default class SessionWizard extends LightningElement {
       });
   }
 
-  // Método para mostrar os registos do Session Venues 
+  // Método para mostrar os registos do Session Venues
   handleShow() {
     this.showTemplateSesssionVenues = true;
   }
-  
+
   handleSuccess(event) {
+    this.dispatchToast("Success!", "The Session record has been successfully saved.", "success");
     updateRecord({ fields: { Id: this.recordId } });
     this.sessionId = event.detail.id;
     let sessionsIdList;
@@ -68,39 +98,60 @@ export default class SessionWizard extends LightningElement {
       sessionsIdList = this.sessions;
     }
     this.sessions = undefined;
-    sessionsIdList.push(event.detail.id);
-    this.sessions = sessionsIdList;
+    //sessionsIdList.push(event.detail.id);
+    //this.sessions = sessionsIdList;
 
     createSessionVenues({
       sessionId: this.sessionId
     })
       .then((result) => {
-        if (result.length > 0) {
-          this.sessionVenues = result;
+        if (result) {
+          sessionsIdList.push(result);
+          this.sessions = sessionsIdList;
         } else {
-          this.sessionVenues = undefined;
+          this.sessions = undefined;
         }
+        this.hideLoading();
       })
       .catch((error) => {
         this.error = error;
+        this.hideLoading();
       });
-    const evt = new ShowToastEvent({
-      title: "Success!",
-      message: "The Session record has been successfully saved.",
-      variant: "success"
-    });
-    this.dispatchEvent(evt);
+
     this.showSessionsSaved = true;
+
     console.table(this.sessions);
     console.table(this.sessionVenues);
   }
-  
-  // Método para seleccionar o espaço 
+
+  // Método para seleccionar o espaço
   selectVenue(event) {
+    this.venueSelected = false;
+    this.venueId = undefined;
+    this.isLoading = true;
     this.venueId = event.currentTarget.dataset.venue;
     this.venues = undefined;
     this.venueSelected = true;
   }
+
+  hideLoading() {
+    this.isLoading = false;
+  }
+
+  showLoading() {
+    this.isLoading = true;
+  }
+
+  dispatchToast(title, message, variant) {
+    const evt = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant
+    });
+    this.dispatchEvent(evt);
+  }
+
+  
 
   /* handleSessionVenue(event) {
     updateRecord({ fields: { Id: this.recordId } });
